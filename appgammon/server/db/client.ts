84de832214671c96d72a24b5a3d25c092b65212c
Dan Huynh <danielryanh7@gmail.com>
2026-02-18 16:51:43 -0500

    oops

--- appgammon/server/db/client.ts
+++ appgammon/server/db/client.ts
@@ -6,12 +6,12 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../.env", override: false });
 
 const pool = new Pool({
-  host: process.env.RDSHOST || "localhost",
+  host: process.env.DB_HOST || "localhost",
   port: 5432,
-  user: process.env.USER || "appgammon",
-  password: process.env.PASSWORD || "password",
-  database: process.env.DBNAME || "appgammon",
-  ssl: process.env.RDSHOST ? { rejectUnauthorized: false } : false,
+  user: process.env.DB_USER || "appgammon",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "appgammon",
+  ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
 });
 
 export const db = drizzle(pool, { schema });
